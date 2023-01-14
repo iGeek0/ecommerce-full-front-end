ab3473b2b8a7d54448620490a812e933d1c43201
iGeek <jesus.cardenas.d@gmail.com>
2023-01-14 00:01:32 -0600

    corte productos carrito

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,24 +1,66 @@
 import PaypalCheckoutButton from "../components/PaypalCheckoutButton";
-const products = [1, 2, 3, 4, 5]
+import { useEffect, useState } from "react";
+// const products = [1, 2, 3, 4, 5]
+
+
 
 const Checkout = () => {
+
+    const [products, setProducts] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    let loadCart = () => {
+        if (localStorage.getItem('cart')) {
+            let cart = JSON.parse(localStorage.getItem('cart'));
+            setTotal(cart.reduce((total, item) => total + item.price * item.quantity, 0));
+            setProducts(cart);
+        }
+    }
+
+    useEffect(() => {
+        document.title = 'Checkout';
+        loadCart();
+    }, []);
+
+    let eliminarProducto = (id) => {
+        console.log("El id es: ", id);
+        // Delete product from cart in local storage if quantity is 1
+
+            let cart = JSON.parse(localStorage.getItem('cart'));
+            cart.forEach((item) => {
+                if (item._id === id) {
+                    if (item.quantity === 1) {
+                        let newCart = cart.filter((item) => item._id !== id);
+                        localStorage.setItem('cart', JSON.stringify(newCart));
+                    } else {
+                        item.quantity -= 1;
+                        localStorage.setItem('cart', JSON.stringify(cart));
+                    }
+                }
+            });
+            alert('Producto eliminado del carrito');
+            loadCart();
+    }
+
     return (
         <div className="p-4">
             <div className="row">
                 <div className="col-md-5">
                     {products.map((product) => {
                         return (
-                            <div className="card mb-3" key={product}>
+                            <div className="card mb-3" key={product._id}>
                                 <div className="row g-0">
                                     <div className="col-md-4">
-                                        <img src="https://picsum.photos/300" className="img-fluid rounded-start h-100" alt="test" />
+                                        <img src={product.image} className="img-fluid rounded-start h-100" alt="test" />
                                     </div>
                                     <div className="col-md-8">
                                         <div className="card-body">
-                                            <h5 className="card-title">Card title</h5>
-                                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content</p>
+                                            <h5 className="card-title">{product.name}</h5>
+                                            <p className="card-text">{product.description}</p>
+                                            <h5 style={{color:"black"}}>${product.price}</h5>
+                                            <p className="card-text"><small className="text-muted">Cantidad: {product.quantity}</small></p>
                                             <div className="text-start">
-                                                <button className="btn btn-danger btn-sm">Eliminar</button>
+                                                <button className="btn btn-danger btn-sm" type="button" onClick={()=>eliminarProducto(product._id)}>Eliminar</button>
                                             </div>
                                         </div>
                                     </div>
@@ -34,14 +76,14 @@ const Checkout = () => {
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                             <div className="row">
                                 <div className="col-md-6">
-                                    <p>Subtotal <b> 0.00</b></p>
+                                    <p>Subtotal <b> {total} USD</b></p>
                                     <p>Envio <b> 0.00</b></p>
-                                    <p>Total <b> 0.00</b></p>
+                                    <p>Total <b> {total} USD</b></p>
                                 </div>
                             </div>
                             <div className="text-center">
                                 <PaypalCheckoutButton currency={"MXN"}
-                                    amount={1}
+                                    amount={total}
                                     showSpinner={false} />
                             </div>
                         </div>
--- src/pages/products/ProductsList.jsx
+++ src/pages/products/ProductsList.jsx
@@ -2,7 +2,7 @@ import Hero from "../../components/Hero";
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useContext } from "react";
 import { getProducts } from "../../services/Product.service";
-import  UserContext  from "../../context/User.context";
+import UserContext from "../../context/User.context";
 
 
 
@@ -21,50 +21,78 @@ const ProductList = () => {
         verifyingToken();
     }, [verifyingToken]);
 
+    let addProductToCart = (product) => {
+        console.log("El producto es: ", product);
+
+        if (localStorage.getItem('cart')) {
+            let cart = JSON.parse(localStorage.getItem('cart'));
+            let productExists = false;
+            cart.forEach((item) => {
+                if (item._id === product._id) {
+                    item.quantity += 1;
+                    productExists = true;
+                }
+            });
+            if (!productExists) {
+                cart.push({ ...product, quantity: 1 });
+            }
+            localStorage.setItem('cart', JSON.stringify(cart));
+            alert('Producto agregado al carrito');
+        } else {
+            localStorage.setItem('cart', JSON.stringify([{ ...product, quantity: 1 }]));
+            alert('Producto agregado al carrito');
+        }
+
+    }
+
+
     return (
         <main>
-                <Hero />
+            <Hero />
 
-                <div className="mt-5 p-5">
-                    <div className="row">
-                        <div className="col-md-2">
-                            <h5>Esto puede ser algun menu o anuncio</h5>
-                            <ul className="list-unstyled mt-3 mb-4">
-                                <li>10 users included</li>
-                                <li>2 GB of storage</li>
-                                <li>Email support</li>
-                                <li>Help center access</li>
-                            </ul>
-                        </div>
-                        <div className="col-md-10">
-                            <div className="row">
-                                {products.map((product) => {
-                                    return (
-                                        <div className="col-sm-6 col-md-4" key={product.id}>
-                                            <div className="card mb-4">
-                                                <img src={product.image} className="card-img-top" alt='test' />
+            <div className="mt-5 p-5">
+                <div className="row">
+                    <div className="col-md-2">
+                        <h5>Esto puede ser algun menu o anuncio</h5>
+                        <ul className="list-unstyled mt-3 mb-4">
+                            <li>10 users included</li>
+                            <li>2 GB of storage</li>
+                            <li>Email support</li>
+                            <li>Help center access</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-10">
+                        <div className="row">
+                            {products.map((product) => {
+                                return (
+                                    <div className="col-sm-6 col-md-4" key={product._id}>
+                                        <div className="card mb-4">
+                                            <img src={product.image} className="card-img-top" alt='test' />
 
-                                                <div className="card-body">
-                                                    <h3>${product.price}</h3>
-                                                    <h4 className='card-title'>{product.name}</h4>
-                                                    <p className="card-text">{product.description}</p>
-                                                    <Link to="/checkout" className="btn btn-primary btn-sm d-grid">Carrito</Link>
-                                                    <Link to={`/detail-product/${product._id}`} className="btn btn-warning btn-sm d-grid mt-2">Detalle</Link>
+                                            <div className="card-body">
+                                                <h3>${product.price}</h3>
+                                                <h4 className='card-title'>{product.name}</h4>
+                                                <p className="card-text">{product.description}</p>
+
+                                                <div class="d-grid gap-2">
+                                                    <button type="button" className="btn btn-primary btn-sm" onClick={() => addProductToCart(product)}>Agregar al carrito</button>
+                                                    <Link to={`/detail-product/${product._id}`} className="btn btn-warning btn-sm">Detalle</Link>
                                                 </div>
                                             </div>
                                         </div>
-                                    )
-                                })}
-                            </div>
-
+                                    </div>
+                                )
+                            })}
                         </div>
-                    </div>
 
+                    </div>
                 </div>
 
+            </div>
 
 
-            </main>
+
+        </main>
     );
 }
 
