0b5b39f74166d6296c51217a104264b37e92c225
iGeek <jesus.cardenas.d@gmail.com>
2023-01-14 12:53:55 -0600

    agregadas categorias

--- src/pages/products/ProductsList.jsx
+++ src/pages/products/ProductsList.jsx
@@ -2,6 +2,7 @@ import Hero from "../../components/Hero";
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { getProducts } from "../../services/Product.service";
+import { getCategories } from "../../services/Category.service";
 
 
 
@@ -10,12 +11,18 @@ import { getProducts } from "../../services/Product.service";
 const ProductList = () => {
 
     const [products, setProducts] = useState([]);
+    const [categorias, setCategorias] = useState([]);
+
     useEffect(() => {
         document.title = 'Productos';
         getProducts().then((res) => {
             setProducts(res.data);
         });
+        getCategories().then((res) => {
+            setCategorias(res.data);
+        });
     }, []);
+
 
 
     let addProductToCart = (product) => {
@@ -50,12 +57,15 @@ const ProductList = () => {
             <div className="mt-5 p-5">
                 <div className="row">
                     <div className="col-md-2">
-                        <h5>Esto puede ser algun menu o anuncio</h5>
-                        <ul className="list-unstyled mt-3 mb-4">
-                            <li>10 users included</li>
-                            <li>2 GB of storage</li>
-                            <li>Email support</li>
-                            <li>Help center access</li>
+                        <h5>Categorias</h5>
+                        <ul className="mt-3 mb-4">
+                            {categorias.map((categoria) => {
+                                return (
+                                    <li key={categoria._id}>
+                                        {categoria.name}
+                                    </li>
+                                )
+                            })}
                         </ul>
                     </div>
                     <div className="col-md-10">
